b8916de4cc1431c19a3c648012593a2b749c71b0
Nick Smith <nickajsmith27@gmail.com>
2023-12-11 15:02:21 -0500

    added sql statements for populating database

--- db-node-app/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE FantasyFootball;
-
-CREATE TABLE Player(
-    PlayerID SERIAL PRIMARY KEY,
-    Pname VARCHAR(255),
-    Position VARCHAR(255),
-    Salary VARCHAR(255),
-    FPoints VARCHAR(255)
-);
