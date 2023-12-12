ac01803eb7142c1dace048c802393635bc3329cc
Drew Amunategui II <drewamunateguiii@Drews-MBP-2.hsd1.md.comcast.net>
2023-12-11 22:49:11 -0500

    refactored all teams component

--- db-react-app/src/App.js
+++ db-react-app/src/App.js
@@ -5,15 +5,10 @@ import {
   Toolbar,
   Typography,
   Button,
-  Container,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-} from '@mui/material';import axios from "axios";
+  Container
+} from '@mui/material';
+import axios from "axios";
+import Allteams from './components/Allteams';
 
 const baseURL =  "http://localhost:3001/api"
 
@@ -21,19 +16,6 @@ const baseURL =  "http://localhost:3001/api"
 function App() {
   const [loading, setLoading] = useState(false);
   const [returnData, setReturnData] = useState(null);
-
-  const handleButtonClick = (content) => {
-    setLoading(content);
-  };
-
-  /*const getAll = () => {
-    const {data} = await axios.get('http://localhost:3001/api/fetch_all')
-    fetch('http://localhost:3001/api/fetch_all')
-      .then(response => response.json())
-      .then(data => setReturnData(data.message))
-      .then(setDisplayContent = true)
-      .catch(error => console.error('Error fetching data: ', error));
-  };*/
 
   const getAll = () => {
     axios.get(`${baseURL}/fetch_all`)
@@ -61,39 +43,8 @@ function App() {
           </Button>
         </div>
         <div style={{ marginTop: '20px' }}>
-          {loading && (
-            <TableContainer component={Paper}>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Team ID</TableCell>
-                    <TableCell>Name</TableCell>
-                    <TableCell>State</TableCell>
-                    <TableCell>Wins</TableCell>
-                    <TableCell>Losses</TableCell>
-                    <TableCell>Made Playoffs</TableCell>
-                    <TableCell>Salary of Players Used</TableCell>
-                    <TableCell>Conference</TableCell>
-                    <TableCell>Division</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {returnData.map((team, index) => (
-                    <TableRow key={index}>
-                      <TableCell>{team.teamid}</TableCell>
-                      <TableCell>{team.name}</TableCell>
-                      <TableCell>{team.state}</TableCell>
-                      <TableCell>{team.win}</TableCell>
-                      <TableCell>{team.loss}</TableCell>
-                      <TableCell>{team.madeplayoffs}</TableCell>
-                      <TableCell>{team.salary_of_players_used}</TableCell>
-                      <TableCell>{team.conference}</TableCell>
-                      <TableCell>{team.division}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
+          {loading && ( 
+            <Allteams data={returnData} />
           )}
         </div>
       </Container>
---
+++ db-react-app/src/components/Allteams.js
@@ -0,0 +1,55 @@
+// App.js
+import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@mui/material';
+
+
+function Allteams(props) {
+
+  const {data} = props;
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Team ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>State</TableCell>
+            <TableCell>Wins</TableCell>
+            <TableCell>Losses</TableCell>
+            <TableCell>Made Playoffs</TableCell>
+            <TableCell>Salary of Players Used</TableCell>
+            <TableCell>Conference</TableCell>
+            <TableCell>Division</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((team, index) => (
+            <TableRow key={index}>
+              <TableCell>{team.teamid}</TableCell>
+              <TableCell>{team.name}</TableCell>
+              <TableCell>{team.state}</TableCell>
+              <TableCell>{team.win}</TableCell>
+              <TableCell>{team.loss}</TableCell>
+              <TableCell>{team.madeplayoffs}</TableCell>
+              <TableCell>{team.salary_of_players_used}</TableCell>
+              <TableCell>{team.conference}</TableCell>
+              <TableCell>{team.division}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+
+export default Allteams;
